9af3aac117aeae7f02abf5299fd4bd968892f66c
dorkusprime <dorkusprime@users.noreply.github.com>
2019-12-28 15:10:00 -0500

    Adding mod keys to all layers, adding `~ key

--- keyboards/dztech/dz65rgb/keymaps/dorkusprime/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/dorkusprime/keymap.c
@@ -66,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,    KC_LALT,    KC_LGUI,                            KC_SPC,                                         KC_RGUI,    KC_RALT,    KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
     [1] = LAYOUT_65_ansi(
-        KC_NO,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_NO,      KC_TRNS,
+        KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_NO,      KC_TRNS,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      MO(2),
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                  KC_NO,
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                  COLORON,    KC_NO,
+        KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,                COLORON,    KC_NO,
         KC_NO,      KC_NO,      KC_NO,                              KC_NO,                                          KC_NO,      KC_NO,      KC_NO,      RGB_RMOD,   COLOROFF,   RGB_MOD
     ),
     [2] = LAYOUT_65_ansi(
